1077ce7a8900a63944292b9f6bcf068647ddbb84
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-12-17 00:09:24 +0300

    New attributes
    maxWidth
    isFH
    shadow
    ariaLabel
    allowedBlocks
    scroll
    visible
    spacing
    link
    shape
    aos

---
+++ assets/src/admin/utils/attributes.js
@@ -0,0 +1,74 @@
+const attributes = {
+    
+    maxWidth: {
+        type: 'number'
+    },
+    isFH: {
+        type: 'boolean',
+        default: false
+    },
+    shadow: {
+        type: 'string'
+    },
+    ariaLabel: {
+        type: 'string'
+    },
+    allowedBlocks: {
+        type: 'array',
+        default: []
+    },
+    scroll: {
+        type: 'object',
+        default: {
+            show: false,
+            target: undefined,
+            color: undefined
+        }
+    },
+    visible: {
+        type: 'object',
+        default: {
+            desktop: false,
+            laptop: false,
+            tablet: false,
+            smartphone: false
+        }
+    },
+    spacing: {
+        type: 'object',
+        default: {
+            desktop: undefined,
+            laptop: undefined,
+            tablet: undefined,
+            smartphone: undefined
+        }
+    },
+    link: {
+        type: 'object',
+        default: {
+            url: undefined,
+            target: false
+        }
+    },
+    shape: {
+        type: 'object',
+        default: {
+            type: undefined,
+            height: 5,
+            color: undefined
+        }
+    },
+    aos: {
+        type: 'object',
+        default: {
+            type: undefined,
+            once: false,
+            offset: 120,
+            duration: 400,
+            delay: 0,
+            ease: 'ease'
+        }
+    }
+};
+
+export default attributes;
