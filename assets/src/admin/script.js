169d2f8dd8823a070a2603b43afa935aa5e48490
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-12-17 00:09:39 +0300

    Imported `container` block

--- assets/src/admin/script.js
+++ assets/src/admin/script.js
@@ -3,4 +3,35 @@
  */
 import './style.css';
 
-console.log('ww');
+/**
+ * Import custom/built-in editor blocks.
+ */
+import * as container from './index.jsx';
+import * as loader from './loader/index.jsx';
+
+export function registerBlocks () {
+	[
+		container,
+		// loader
+	].forEach( block => {
+
+		if ( ! block ) {
+			return;
+		} // End If Statement
+
+		const { name, category, icon, settings } = block;
+
+		wp.blocks.registerBlockType( 
+			`my/${ name }`, 
+			{ 
+				category, 
+				icon: { 
+					src: icon
+				}, 
+				...settings 
+			} 
+		);
+	} );
+};
+
+registerBlocks();
