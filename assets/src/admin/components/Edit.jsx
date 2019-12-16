5be599bce538736dcb745967b83d8664d70bd41d
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-12-17 00:13:17 +0300

    Display innerBlocks

---
+++ assets/src/admin/components/Edit.jsx
@@ -0,0 +1,77 @@
+/**
+ * Block dependencies
+ */
+import AOS from 'aos';
+import escape from 'lodash.escape';
+import includes from 'lodash/includes';
+import classnames from 'classnames';
+import { keyframes } from 'styled-components';
+// import Controls from './Controls.jsx';
+import Inspector from './Inspector.jsx';
+import shapes from './../utils/shapes.jsx';
+import applyWithColors from './../utils/withColors';
+import applyWithSelect from './../utils/withSelect';
+
+/**
+ * Internal block libraries
+ */
+const { Fragment, Component } = wp.element;
+const { compose } = wp.compose;
+const { InnerBlocks } = wp.blockEditor;
+
+export default compose( applyWithColors, applyWithSelect ) ( class Edit extends Component {
+
+	constructor( props ) {
+		super( ...arguments );
+	}
+
+	render() {
+
+		const { 
+			hasInnerBlocks,
+			className,
+			isSelected,
+			attributes,
+			setAttributes } = this.props;
+
+		const { 
+			maxWidth,
+			allowedBlocks } = attributes;
+
+		return (
+			<Fragment>
+				{ isSelected && (
+					<Inspector
+						{ ...this.props }
+					/>
+				) }
+				<div 
+                    className={ classnames( 
+                    	className
+                	) }
+                >
+                	<div 
+                		className={ classnames( 
+                			'wp-block-my-container__wrapper'
+	                	) }
+                		style={ {
+                			maxWidth: maxWidth  ?  `${ parseInt( maxWidth ) }px`  :  undefined
+						} }
+                	>
+                		{ ( Array.isArray( allowedBlocks ) && allowedBlocks.length ) ? (
+                			<InnerBlocks 
+			    				templateLock={ false }
+			    				allowedBlocks={ allowedBlocks }
+			    				renderAppender={ ! hasInnerBlocks && InnerBlocks.ButtonBlockAppender }
+			    			/>
+                		) : 
+                			<InnerBlocks 
+			    				renderAppender={ ! hasInnerBlocks && InnerBlocks.ButtonBlockAppender }
+			    			/>
+                		}
+		    		</div>
+				</div>
+			</Fragment>
+		);
+	}
+} );
