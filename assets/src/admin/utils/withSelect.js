912787e6690c4613eb8b4e62425f44ef1708c490
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-12-17 12:08:16 +0300

    Get theme’s color palette

--- assets/src/admin/utils/withSelect.js
+++ assets/src/admin/utils/withSelect.js
@@ -7,10 +7,14 @@ const { withSelect } = wp.data;
  * Generate block data.
  */
 const applyWithSelect = withSelect( ( select, { clientId } ) => {
-    const { getBlock } = select( 'core/block-editor' );
-    const block = getBlock( clientId )
+    const { 
+    	getBlock,
+    	getSettings } = select( 'core/block-editor' );
+    	
+    const block = getBlock( clientId );
 
     return {
+    	colors: getSettings().colors,
         hasInnerBlocks: !! ( block && block.innerBlocks.length )
     };
 } );
