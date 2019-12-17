8a47ce6c623ff5c42ee5144f134edb4426f703ba
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-12-17 12:08:32 +0300

    Default (placeholder) colors

---
+++ assets/src/admin/utils/colors.jsx
@@ -0,0 +1,40 @@
+const colors = [];
+
+// White
+colors.push( {
+	name: 'White', 
+	slug: 'white', 
+	color: '#FFFFFF'
+} );
+// Black
+colors.push( {
+	name: 'Black', 
+	slug: 'black', 
+	color: '#000000'
+} );
+// Blue
+colors.push( {
+	name: 'Blue', 
+	slug: 'blue', 
+	color: '#59BACC'
+} );
+// Green
+colors.push( {
+	name: 'Green', 
+	slug: 'green', 
+	color: '#58AD69'
+} );
+// Orange
+colors.push( {
+	name: 'Orange', 
+	slug: 'orange', 
+	color: '#FFBC49'
+} );
+// Red
+colors.push( {
+	name: 'Red', 
+	slug: 'red', 
+	color: '#E2574C'
+} );
+
+export default colors;
