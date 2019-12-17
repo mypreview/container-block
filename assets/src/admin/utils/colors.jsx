e53a25f35cc2169716f7037c2f1925783f1fe3f1
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-12-17 12:15:33 +0300

    Make color names translatable

--- assets/src/admin/utils/colors.jsx
+++ assets/src/admin/utils/colors.jsx
@@ -1,38 +1,43 @@
+/**
+ * Internal block libraries
+ */
+const { _x } = wp.i18n;
+
 const colors = [];
 
 // White
 colors.push( {
-	name: 'White', 
+	name: _x( 'White', 'color pallet', 'container-block' ), 
 	slug: 'white', 
 	color: '#FFFFFF'
 } );
 // Black
 colors.push( {
-	name: 'Black', 
+	name: _x( 'Black', 'color pallet', 'container-block' ), 
 	slug: 'black', 
 	color: '#000000'
 } );
 // Blue
 colors.push( {
-	name: 'Blue', 
+	name: _x( 'Blue', 'color pallet', 'container-block' ), 
 	slug: 'blue', 
 	color: '#59BACC'
 } );
 // Green
 colors.push( {
-	name: 'Green', 
+	name: _x( 'Green', 'color pallet', 'container-block' ), 
 	slug: 'green', 
 	color: '#58AD69'
 } );
 // Orange
 colors.push( {
-	name: 'Orange', 
+	name: _x( 'Orange', 'color pallet', 'container-block' ),
 	slug: 'orange', 
 	color: '#FFBC49'
 } );
 // Red
 colors.push( {
-	name: 'Red', 
+	name: _x( 'Red', 'color pallet', 'container-block' ),
 	slug: 'red', 
 	color: '#E2574C'
 } );
