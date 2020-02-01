67f00e7cbee03cf70e649b53cfe7d583306ebe1d
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-02-01 13:45:08 +0300

    Compiled

--- assets/dist/admin/script.asset.php
+++ assets/dist/admin/script.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wp-element', 'wp-polyfill'), 'version' => 'de1861e55c036d658b71ff7d6722f9fc');
+<?php return array('dependencies' => array('jquery', 'react', 'wp-element', 'wp-polyfill'), 'version' => '5981147d9928495a05ffbe059c012740');
